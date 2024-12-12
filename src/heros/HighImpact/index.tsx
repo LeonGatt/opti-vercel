7a38cfea28f3123bd0f8af9e908b026eb23e1b9f
Ron Robertson <ron.robertson@naturalpoint.com>
2024-12-12 08:15:23 -0800

    Updating style for heros and blocks

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
 import React, { useEffect } from 'react'
 
 import type { Page } from '@/payload-types'
@@ -7,21 +5,22 @@ import type { Page } from '@/payload-types'
 import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
+import styles from './index.module.css'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
-  const { setHeaderTheme } = useHeaderTheme()
-
-  useEffect(() => {
-    setHeaderTheme('dark')
-  })
 
   return (
-    <div className="relative -mt-[10.4rem] flex items-end text-white" data-theme="dark">
-      <div className="container mb-8 z-10 relative">
-        <div className="max-w-[34rem]">
-          {richText && <RichText className="mb-6" content={richText} enableGutter={false} />}
+    <div className={ styles.heroHigh }>
+      <div className={ styles.contentWrapper }>
+        <div className={ styles.visualWrapper}>
+          {media && typeof media === 'object' && (
+            <Media fill className={ styles.imageWrapper } priority resource={media} />
+          )}
+        </div>
+        <div className={ styles.copyWrapper }>
+          {richText && <RichText content={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex gap-4">
+            <ul className={ styles.linksList }>
               {links.map(({ link }, i) => {
                 return (
                   <li key={i}>
@@ -32,14 +31,6 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
             </ul>
           )}
         </div>
-      </div>
-      <div className="min-h-[80vh] select-none">
-        {media && typeof media === 'object' && (
-          <React.Fragment>
-            <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
-            <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent" />
-          </React.Fragment>
-        )}
       </div>
     </div>
   )
