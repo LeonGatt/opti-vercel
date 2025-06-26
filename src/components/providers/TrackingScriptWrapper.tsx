1b5a634b66283d625d8e5ab6f3dd589871484eac
Siobahn Schuck <siobahn.schuck@planar.com>
2025-06-26 08:09:22 -0700

    GTM Scripts for Environments (#19)
    * feat(add): Auth var for gtm
    
    * fix(var): use next_pub prefix

--- src/components/providers/TrackingScriptWrapper.tsx
+++ src/components/providers/TrackingScriptWrapper.tsx
@@ -22,7 +22,7 @@ const TrackingScriptWrapper = ({
 }
 
 const GTMScript = () => {
-  if (!process.env.GTM_ID) {
+  if (!process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID) {
     return null
   }
 
@@ -34,8 +34,8 @@ const GTMScript = () => {
        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', '${process.env.GTM_ID}');
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${process.env.NEXT_PUBLIC_GTM_AUTH}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
       `}
     />
   )
@@ -74,7 +74,7 @@ export const TrackingScriptsBody = () => {
       {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}&gtm_auth=${process.env.NEXT_PUBLIC_GTM_AUTH}&gtm_cookies_win=x`}
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
