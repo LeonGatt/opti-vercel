6f1c62ce4936fbd65760cb0ceeae00150cbcb7c4
Marek Fiala <marek.fiala@optitrack.com>
2025-07-23 16:47:17 +0200

    fix(Header): adjust styles and remove custom pointers #OPT-188 (#75)

--- src/globals/Header/navbar/navbar5-custom.tsx
+++ src/globals/Header/navbar/navbar5-custom.tsx
@@ -76,7 +76,7 @@ const Navbar5: React.FC<{
                 <MenuIcon strokeWidth={1} />
               </Button>
             </SheetTrigger>
-            <SheetContent side="left" className="max-h-screen overflow-scroll bg-[#f1f1f1] dark:bg-[#303030] flex flex-col justify-between px-10 py-32">
+            <SheetContent side="left" className="max-h-screen bg-[#f1f1f1] dark:bg-[#303030] flex flex-col justify-between px-10 py-32">
               <SheetHeader className="hidden">
                 <SheetTitle>
                   <div className="flex items-center">
@@ -98,7 +98,7 @@ const Navbar5: React.FC<{
                             publicContext={publicContext}
                             {...item.link}
                             className={cn(
-                              "font-light text-2xl",
+                              "font-light text-2xl hover:text-button-primary",
                               isActive && "font-bold"
                             )}
                           />
