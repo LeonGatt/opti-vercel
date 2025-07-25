f426717d875c1f3eb599049c5ce13967d1bb0fa7
Veronika K. <79855797+veronika-kalaskova@users.noreply.github.com>
2025-07-25 08:49:48 +0200

    bugfix: OPT-86 no margin for tabs (#89)

--- src/blocks/Feature/feature54-custom.tsx
+++ src/blocks/Feature/feature54-custom.tsx
@@ -10,7 +10,6 @@ import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
 import { CMSLink } from '@/components/Link'
 import { cn } from '@/utilities'
-import { getSpacings } from '@/utilities/spacings'
 
 type FeatureUSPItem = NonNullable<FeatureBlock['USPs']>[number]
 
@@ -25,58 +24,49 @@ const ActiveTabContent: React.FC<{
     takeFirst: true,
   })
 
-  const { firstNode: firstNodeDescription, rest: restDescription } =
-    splitRichText(richtext, {
-      splitOn: ['h2', 'h3', 'h4'],
-      takeFirst: true,
-    })
+  const { firstNode: firstNodeDescription, rest: restDescription } = splitRichText(richtext, {
+    splitOn: ['h2', 'h3', 'h4'],
+    takeFirst: true,
+  })
 
   return (
-    <div
-      key={feature.id}
-      className="mx-auto mt-12 max-w-[426px] md:max-w-[564px]"
-    >
-      {
-        firstNodeDescription &&
-        fillFromDescription && extractPlainText(firstNodeDescription) && (
-          <RichText
-            publicContext={publicContext}
-            content={firstNodeDescription}
-            overrideStyle={{
-              h2: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
-              h3: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
-              h4: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
-              p: 'line-clamp-4 text-center text-[#86868B] leading-6 text-base',
-            }}
-            withWrapper={false}
-          />
-        )}
-      {
-        restDescription &&
-        fillFromDescription && extractPlainText(restDescription) && (
-          <RichText
-            publicContext={publicContext}
-            content={restDescription}
-            overrideStyle={{
-              p: 'line-clamp-4 text-center text-[#86868B] leading-6 text-base',
-            }}
-            withWrapper={false}
-          />
-        )}
-      {
-        firstNode && !fillFromDescription && extractPlainText(firstNode) && (
-          <RichText
-            publicContext={publicContext}
-            content={firstNode}
-            overrideStyle={{
-              h2: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
-              h3: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
-              h4: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
-              p: 'line-clamp-4 text-center text-[#86868B] leading-6 text-base',
-            }}
-            withWrapper={false}
-          />
-        )}
+    <div key={feature.id} className="mx-auto mt-12 max-w-[426px] px-6 md:max-w-[564px] md:px-8">
+      {firstNodeDescription && fillFromDescription && extractPlainText(firstNodeDescription) && (
+        <RichText
+          publicContext={publicContext}
+          content={firstNodeDescription}
+          overrideStyle={{
+            h2: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
+            h3: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
+            h4: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
+            p: 'line-clamp-4 text-center text-[#86868B] leading-6 text-base',
+          }}
+          withWrapper={false}
+        />
+      )}
+      {restDescription && fillFromDescription && extractPlainText(restDescription) && (
+        <RichText
+          publicContext={publicContext}
+          content={restDescription}
+          overrideStyle={{
+            p: 'line-clamp-4 text-center text-[#86868B] leading-6 text-base',
+          }}
+          withWrapper={false}
+        />
+      )}
+      {firstNode && !fillFromDescription && extractPlainText(firstNode) && (
+        <RichText
+          publicContext={publicContext}
+          content={firstNode}
+          overrideStyle={{
+            h2: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
+            h3: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
+            h4: 'mb-2 line-clamp-3 break-words text-lg font-medium md:mb-3 md:text-xl lg:text-2xl font-heading',
+            p: 'line-clamp-4 text-center text-[#86868B] leading-6 text-base',
+          }}
+          withWrapper={false}
+        />
+      )}
       {rest && !fillFromDescription && extractPlainText(firstNode) && (
         <RichText
           publicContext={publicContext}
@@ -87,19 +77,20 @@ const ActiveTabContent: React.FC<{
           withWrapper={false}
         />
       )}
-      {feature.links && feature.links.length > 0 && <div className={cn("mt-12 flex flex-col justify-center gap-4 md:flex-row md:items-center md:gap-6", feature.links?.length === 1 && 'items-center')}>
-        {feature.links?.map((link) => {
-          return (
-            <CMSLink
-              key={link.id}
-              {...link.link}
-              publicContext={publicContext}
-            />
-          )
-        })}
-      </div>}
+      {feature.links && feature.links.length > 0 && (
+        <div
+          className={cn(
+            'mt-12 flex flex-col justify-center gap-4 md:flex-row md:items-center md:gap-6',
+            feature.links?.length === 1 && 'items-center',
+          )}
+        >
+          {feature.links?.map((link) => {
+            return <CMSLink key={link.id} {...link.link} publicContext={publicContext} />
+          })}
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 const Feature54Custom: React.FC<FeatureBlock & { publicContext: PublicContextProps }> = ({
@@ -108,34 +99,40 @@ const Feature54Custom: React.FC<FeatureBlock & { publicContext: PublicContextPro
   USPs,
   publicContext,
   fillFromDescription,
-  spacings,
 }) => {
-  const tabsData = useMemo(() => USPs?.filter((usp) => usp.id).map((usp) => {
-    return {
-      id: usp.id as string,
-      richText: usp.richText,
-      image: usp.image as MediaType,
-      tagline: usp.tagline,
-      links: usp.links,
-    }
-  }), [USPs])
+  const tabsData = useMemo(
+    () =>
+      USPs?.filter((usp) => usp.id).map((usp) => {
+        return {
+          id: usp.id as string,
+          richText: usp.richText,
+          image: usp.image as MediaType,
+          tagline: usp.tagline,
+          links: usp.links,
+        }
+      }),
+    [USPs],
+  )
 
   const [activeTab, setActiveTab] = useState<FeatureUSPItem | null>(() => tabsData?.[0] || null)
 
-  const selectActiveFeature = useCallback((tabId: string) => {
-    const feature = tabsData?.find((feature) => feature.id === tabId)
-    if (feature) {
-      setActiveTab(feature)
-    }
-  }, [tabsData])
+  const selectActiveFeature = useCallback(
+    (tabId: string) => {
+      const feature = tabsData?.find((feature) => feature.id === tabId)
+      if (feature) {
+        setActiveTab(feature)
+      }
+    },
+    [tabsData],
+  )
 
   if (!tabsData?.length || !activeTab?.id)
     return <div className="text-red-500">You need to add USPs for the component to work</div>
 
   return (
-    <section className={cn('mx-auto py-16 lg:max-w-[1280px]', getSpacings(spacings))}>
-      <div className="mx-6 md:mx-8">
-        <div className="flex justify-center">
+    <section className="mx-auto py-16 lg:max-w-[1280px]">
+      <div>
+        <div className="mx-6 flex justify-center md:mx-8">
           <h3 className="text-text-default font-heading mb-12 text-center text-5xl font-bold lg:max-w-3xl">
             {tagline}
           </h3>
@@ -143,11 +140,11 @@ const Feature54Custom: React.FC<FeatureBlock & { publicContext: PublicContextPro
         <div className="text-center">
           <Tabs value={activeTab.id} onValueChange={selectActiveFeature}>
             {tabsData.map((feature) => (
-              <TabsContent key={feature.id} value={feature.id}>
+              <TabsContent key={feature.id} value={feature.id} className="mx-6 md:mx-8">
                 {feature.image && (
                   <Media
                     resource={feature.image}
-                    imgClassName="w-full object-contain object-center rounded-xl h-[233px] md:h-[389px] lg:h-[612px]"
+                    imgClassName="w-full object-cover object-center rounded-xl h-[233px] md:h-[389px] lg:h-[612px]"
                     htmlElement={null}
                   />
                 )}
@@ -155,7 +152,7 @@ const Feature54Custom: React.FC<FeatureBlock & { publicContext: PublicContextPro
             ))}
             <div className="mt-12">
               <div className="no-scrollbar overflow-auto">
-                <TabsList className="bg-background-dark h-10 p-1">
+                <TabsList className="bg-background-dark mx-6 h-10 p-1 md:mx-8">
                   {tabsData.map((feature) => (
                     <TabsTrigger
                       key={feature.id}
@@ -167,14 +164,14 @@ const Feature54Custom: React.FC<FeatureBlock & { publicContext: PublicContextPro
                   ))}
                 </TabsList>
               </div>
-              {activeTab &&
+              {activeTab && (
                 <ActiveTabContent
                   feature={activeTab}
                   richtext={richText}
                   publicContext={publicContext}
                   fillFromDescription={fillFromDescription}
                 />
-              }
+              )}
             </div>
           </Tabs>
         </div>
